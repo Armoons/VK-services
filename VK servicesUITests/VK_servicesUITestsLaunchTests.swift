022129c04cba2d1420f74a85c877947962acb736
Hujanja <arman11102@mail.ru>
2023-02-17 15:03:37 +0300

    Initial Commit

---
+++ VK servicesUITests/VK_servicesUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  VK_servicesUITestsLaunchTests.swift
+//  VK servicesUITests
+//
+//  Created by Stepanyan Arman  on 17.02.2023.
+//
+
+import XCTest
+
+final class VK_servicesUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
